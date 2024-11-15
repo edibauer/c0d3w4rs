39ce2d46730a8b54945ec9e7cf43b49edc7be9ff
galax0r <edibaldo_369@proton.me>
2024-11-14 21:51:12 -0600

    feat: Creating a static method and instanciating it in main

---
+++ java/Function2/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Result: %d\n", Square.square(3));
+    }
+}
---
+++ java/Function2/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    // att
+
+    // methods
+    public static int square(int num) {
+        return num * num;
+    }
+}
