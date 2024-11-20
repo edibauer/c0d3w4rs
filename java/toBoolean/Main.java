21e3df9515112ec35d3bec7a3590fe25e8383650
edibaldo <edibaldo_369@proton.me>
2024-11-19 23:01:09 -0600

    Feat: Adding new exercicse - Return yes or false

---
+++ java/toBoolean/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+         */
+
+        System.out.println(YesOrNo.boolToWord(false));
+    }
+}
---
+++ java/toBoolean/YesOrNo.java
@@ -0,0 +1,8 @@
+public class YesOrNo {
+    // att
+    // method
+    public static String boolToWord(boolean b) {
+        if ( b == true ) return "Yes";
+        return "No";
+    }
+}
